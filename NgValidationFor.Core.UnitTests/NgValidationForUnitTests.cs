a9ad9a873844e193f51b5490d8b1ca10991d2a64
johnnyreilly <johnny_reilly@hotmail.com>
2015-05-17 19:29:57 +0100

    Added StringLengthDirectiveMapper

--- NgValidationFor.Core.UnitTests/NgValidationForUnitTests.cs
+++ NgValidationFor.Core.UnitTests/NgValidationForUnitTests.cs
@@ -10,19 +10,46 @@ namespace NgValidationFor.UnitTests
     {
         [Required]
         public string RequiredField { get; set; }
+
+        [StringLength(5)]
+        public string NgMaxLengthField { get; set; }
+
+        [StringLength(7, MinimumLength = 3)]
+        public string NgMaxAndMinLengthField { get; set; }
     }
 
     [TestClass]
     public class NgValidationForUnitTests
     {
         [TestMethod]
-        public void NgValidationFor_GetAttributes_returns_required()
+        public void Given_RequiredAttribute_NgValidationFor_returns_required()
         {
-            var dummyModel = new DummyModel{ RequiredField = "JKS"};
+            var dummyModel = new DummyModel { RequiredField = "JKS" };
 
             var result = NgValidationFor.Core.NgValidationForHelper.NgValidationFor(dummyModel, x => x.RequiredField);
-            
+
             Assert.AreEqual("required=\"required\"", result);
+        }
+
+        [TestMethod]
+        public void Given_StringLengthAttribute_with_max_length_NgValidationFor_returns_ng_maxlength()
+        {
+            var dummyModel = new DummyModel { NgMaxLengthField = "JKS" };
+
+            var result = NgValidationFor.Core.NgValidationForHelper.NgValidationFor(dummyModel, x => x.NgMaxLengthField);
+
+            Assert.AreEqual("ng-maxlength=\"5\"", result);
+        }
+
+        [TestMethod]
+        public void Given_StringLengthAttribute_with_max_length_and_with_max_length_NgValidationFor_returns_ng_maxlength_and_ng_minlength()
+        {
+            var dummyModel = new DummyModel { NgMaxAndMinLengthField = "JKS" };
+
+            var result = NgValidationFor.Core.NgValidationForHelper.NgValidationFor(dummyModel, x => x.NgMaxAndMinLengthField);
+
+            Assert.IsTrue(result.Contains("ng-maxlength=\"7\""));
+            Assert.IsTrue(result.Contains("ng-minlength=\"3\""));
         }
     }
 }
---
+++ NgValidationFor.Core/Directives/StringLengthDirectiveMapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace NgValidationFor.Core.Directives
+{
+    public class StringLengthDirectiveMapper : DirectiveMapperBase<StringLengthAttribute>
+    {
+        public override Func<CustomAttributeData, string> MapperFunction
+        {
+            get
+            {
+                return customAttributeData =>
+                {
+                    var validation = "";
+
+                    if (customAttributeData.ConstructorArguments.Count > 0)
+                        validation += "ng-maxlength=\"" + customAttributeData.ConstructorArguments[0].Value + "\"";
+
+                    var minLengthAttribute = customAttributeData.NamedArguments
+                            .Where(x => x.MemberName == "MinimumLength")
+                            .Cast<CustomAttributeNamedArgument?>()
+                            .FirstOrDefault();
+                    if (minLengthAttribute != null)
+                        validation += "ng-minlength=\"" + minLengthAttribute.Value.TypedValue.Value + "\"";
+
+                    return validation;
+                };
+            }
+        }
+    }
+}
